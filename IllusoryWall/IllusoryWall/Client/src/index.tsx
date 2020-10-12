c6d5a47f84ef873676c83f74fa5756de48cd3e45
link00000000 <crandall.logan@gmail.com>
2020-10-12 18:03:02 -0400

    Add react router

--- IllusoryWall/IllusoryWall/Client/src/index.tsx
+++ IllusoryWall/IllusoryWall/Client/src/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
+const baseUrl = document
+    .getElementsByTagName('base')[0]
+    .getAttribute('href') as string
+
 ReactDOM.render(
-    <React.StrictMode>
+    <BrowserRouter basename={baseUrl}>
         <App />
-    </React.StrictMode>,
+    </BrowserRouter>,
     document.getElementById('root')
 )
 
