30749e639fc2a095cbd5867f015eadd5197d2bf2
link00000000 <crandall.logan@gmail.com>
2020-10-16 17:24:10 -0400

    Add initial migration

---
+++ IllusoryWall/IllusoryWall/Migrations/20201016212321_InitialCreate.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IllusoryWall.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enemies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Respawns = table.Column<bool>(nullable: false),
+                    Class = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enemies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Damages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EnemyId = table.Column<int>(nullable: true),
+                    DamageType = table.Column<string>(nullable: false),
+                    Category = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Damages", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Damages_Enemies_EnemyId",
+                        column: x => x.EnemyId,
+                        principalTable: "Enemies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drops",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EnemyId = table.Column<int>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Rate = table.Column<double>(nullable: false),
+                    Location = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drops", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Drops_Enemies_EnemyId",
+                        column: x => x.EnemyId,
+                        principalTable: "Enemies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    EnemyId = table.Column<int>(nullable: true),
+                    HP = table.Column<int>(nullable: false),
+                    Souls = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Locations_Enemies_EnemyId",
+                        column: x => x.EnemyId,
+                        principalTable: "Enemies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Damages_EnemyId",
+                table: "Damages",
+                column: "EnemyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drops_EnemyId",
+                table: "Drops",
+                column: "EnemyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_EnemyId",
+                table: "Locations",
+                column: "EnemyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Damages");
+
+            migrationBuilder.DropTable(
+                name: "Drops");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "Enemies");
+        }
+    }
+}
