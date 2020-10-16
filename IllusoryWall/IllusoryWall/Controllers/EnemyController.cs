aae270599f2c3bda52319d64514f4d5ad47583f6
Mason C. Roberts <mcr66@zips.uakron.edu>
2020-10-16 15:21:34 -0400

    Add endpoint to retrieve enemy

--- IllusoryWall/IllusoryWall/Controllers/EnemyController.cs
+++ IllusoryWall/IllusoryWall/Controllers/EnemyController.cs
@@ -31,6 +31,28 @@ namespace IllusoryWall.Controllers
         }
 
         /// <summary>
+        ///     Endpoint to  retrieve enemies from the database
+        /// </summary>
+        /// <param name="enemyName">Name of the enemy to search for</param>
+        /// <returns>The results of the query with response code</returns>
+        [HttpGet]
+        [Route("{enemyName}")]
+        public IActionResult GetEnemy(string enemyName)
+        {
+            // get results
+            ICollection<Enemy> Results = _context.Enemies
+                                                 .Where(p => p.Name == enemyName)
+                                                 .ToArray();
+
+            // if no results return with 404
+            if(!Results.Any())
+                return NotFound(Results);
+
+            // if results return json with 200
+            return Ok(Results);
+        }
+
+        /// <summary>
         ///     Endpoint to insert new enemies into the database
         /// </summary>
         /// <param name="enemy">Enemy model object to insert into database</param>
