ae6822c667d5267dc6a4fb69707d710cfeb98aab
link00000000 <crandall.logan@gmail.com>
2020-10-24 16:23:55 -0400

    Remove KebabToCamelCase util

--- IllusoryWall/IllusoryWall/Client/src/Utils/KebabToCamelCase.ts
+++
@@ -1,6 +0,0 @@
-export default function (camelCaseString: string) {
-    return camelCaseString
-        .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
-        .toLowerCase()
-        .substring(1)
-}
