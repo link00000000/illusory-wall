00cf946b887840c7dd2634a51c380637a7156375
link00000000 <crandall.logan@gmail.com>
2020-10-26 22:13:18 -0400

    Add NavigationMenu

---
+++ IllusoryWall/IllusoryWall/Client/src/Components/NavigationMenu.tsx
@@ -0,0 +1,55 @@
+import { Menu } from 'antd'
+import React, { Component } from 'react'
+import { NavLink, RouteComponentProps, withRouter } from 'react-router-dom'
+
+interface IProps extends RouteComponentProps {}
+type IState = {}
+
+class _NavigationMenu extends Component<IProps, IState> {
+    static displayName = _NavigationMenu.name
+
+    private static readonly navigationItems: { [key: string]: string } = {
+        Home: '/',
+        Add: '/add',
+        Update: '/update',
+        Remove: '/remove'
+    }
+
+    private get basePath(): string {
+        const pathname = this.props.location.pathname
+        const endIndex = pathname.indexOf('/', 1)
+        if (endIndex === -1) {
+            return pathname
+        }
+
+        return pathname.substring(0, endIndex)
+    }
+
+    constructor(props: IProps) {
+        super(props)
+
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                <Menu
+                    mode='vertical-left'
+                    theme='dark'
+                    selectedKeys={[this.basePath]}
+                >
+                    {Object.entries(_NavigationMenu.navigationItems).map(
+                        ([displayName, route]) => (
+                            <Menu.Item key={route}>
+                                <NavLink to={route}>{displayName}</NavLink>
+                            </Menu.Item>
+                        )
+                    )}
+                </Menu>
+            </div>
+        )
+    }
+}
+
+export const NavigationMenu = withRouter(_NavigationMenu)
--- IllusoryWall/IllusoryWall/Client/src/Layouts/DefaultLayout.tsx
+++ IllusoryWall/IllusoryWall/Client/src/Layouts/DefaultLayout.tsx
@@ -1,4 +1,7 @@
+import { Layout } from 'antd'
 import React, { Component } from 'react'
+import { NavigationMenu } from '../Components/NavigationMenu'
+import styles from './DefaultLayout.module.css'
 
 type IProps = {}
 type IState = {}
@@ -9,8 +12,12 @@ export class DefaultLayout extends Component<IProps, IState> {
     render() {
         return (
             <div>
-                HEADER
-                {this.props.children}
+                <Layout className={styles['layout']}>
+                    <Layout.Sider className={styles['layout-sider']}>
+                        <NavigationMenu />
+                    </Layout.Sider>
+                    <Layout.Content>{this.props.children}</Layout.Content>
+                </Layout>
             </div>
         )
     }
