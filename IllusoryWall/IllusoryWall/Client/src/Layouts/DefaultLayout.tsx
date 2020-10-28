b58753771f5da98f195c6c535ee7f5f276723f8b
link00000000 <crandall.logan@gmail.com>
2020-10-27 21:04:11 -0400

    Make navmeny collapsable

--- IllusoryWall/IllusoryWall/Client/src/Layouts/DefaultLayout.tsx
+++ IllusoryWall/IllusoryWall/Client/src/Layouts/DefaultLayout.tsx
@@ -5,25 +5,55 @@ import { NavigationMenu } from '../Components/NavigationMenu'
 import styles from './DefaultLayout.module.css'
 
 type IProps = {}
-type IState = {}
+type IState = {
+    collapsed: boolean
+}
 
 export class DefaultLayout extends Component<IProps, IState> {
     static displayName = DefaultLayout.name
 
+    constructor(props: IProps | Readonly<IProps>) {
+        super(props)
+
+        this.handleCollapse = this.handleCollapse.bind(this)
+
+        this.state = {
+            collapsed: false
+        }
+    }
+
+    private handleCollapse(collapsed: boolean) {
+        this.setState({ collapsed })
+    }
+
     render() {
         return (
             <div>
-                <Layout className={styles['layout']}>
-                    <Layout.Header className={styles['layout-header']}>
+                <Layout
+                    className={styles['layout']}
+                    style={this.state.collapsed ? { marginLeft: 80 } : {}}
+                >
+                    <Layout.Sider
+                        className={styles['layout-sider']}
+                        collapsible
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.handleCollapse}
+                    >
+                        <NavigationMenu />
+                    </Layout.Sider>
+                    <Layout.Header
+                        className={
+                            this.state.collapsed
+                                ? styles['layout-header-collapsed']
+                                : styles['layout-header']
+                        }
+                    >
                         <Row>
                             <Col span={12} offset={6}>
                                 <EnemySearch />
                             </Col>
                         </Row>
                     </Layout.Header>
-                    <Layout.Sider className={styles['layout-sider']}>
-                        <NavigationMenu />
-                    </Layout.Sider>
                     <Layout.Content className={styles['layout-content']}>
                         {this.props.children}
                     </Layout.Content>
