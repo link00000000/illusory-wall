07b722a3104aaa3eac28685adac2522b7a031a0b
link00000000 <crandall.logan@gmail.com>
2020-10-29 15:41:52 -0400

    Add button to search bar

--- IllusoryWall/IllusoryWall/Client/src/Components/EnemySearch.tsx
+++ IllusoryWall/IllusoryWall/Client/src/Components/EnemySearch.tsx
@@ -1,10 +1,15 @@
-import { Input } from 'antd'
+import { PlusOutlined } from '@ant-design/icons'
+import { Button, Input } from 'antd'
 import React, { Component } from 'react'
+import styles from './EnemySearch.module.css'
 
 type IProps = {
     disabled?: boolean
 }
-type IState = {}
+type IState = {
+    name: string
+    loading: boolean
+}
 
 export class EnemySearch extends Component<IProps, IState> {
     static displayName = EnemySearch.name
@@ -12,16 +17,43 @@ export class EnemySearch extends Component<IProps, IState> {
     constructor(props: IProps) {
         super(props)
 
-        this.state = {}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+        this.state = {
+            name: '',
+            loading: false
+        }
+    }
+
+    private handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        this.setState({ name: event.currentTarget.value })
+    }
+
+    private handleSubmit(_event: any): void {
+        console.log(this.state.name)
     }
 
     render() {
         return (
-            <div>
+            <div className={styles['search']}>
                 <Input
                     disabled={this.props.disabled}
                     placeholder='Type to search for enemy by name'
+                    allowClear
+                    onChange={this.handleChange}
+                    onSubmit={this.handleSubmit}
+                    value={this.state.name}
+                    className={styles['search-bar']}
                 />
+                <Button
+                    type='primary'
+                    className={styles['submit']}
+                    icon={<PlusOutlined />}
+                    loading={this.state.loading}
+                >
+                    Add
+                </Button>
             </div>
         )
     }
