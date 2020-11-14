b968ae127eb591d33bf5b30a01f0e53dbba387e7
link00000000 <crandall.logan@gmail.com>
2020-11-13 21:43:37 -0500

    Add scraping for enemy name

--- IllusoryWall/WebScraper/WebScraper.cs
+++ IllusoryWall/WebScraper/WebScraper.cs
@@ -1,5 +1,47 @@
 ﻿using System;
+using ScrapySharp.Network;
+using ScrapySharp.Extensions;
+using System.Linq;
+using HtmlAgilityPack;
 
-namespace WebScraper {
-    public class Scraper { }
+namespace WebScraper
+{
+    // Scraping results POCO object
+    public class ScraperResult
+    {
+        public string Name { get; set; }
+    }
+
+    public static class Scraper
+    {
+        // Scrape a page at a Url
+        public static ScraperResult scrape(Uri url)
+        {
+            // Start ScrapySharp pseudo-browser
+            var browser = new ScrapingBrowser();
+            WebPage page = browser.NavigateToPage(url, HttpVerb.Get);
+
+            var infoBox = _infoBox(page);
+
+            // Map results to object
+            return new ScraperResult
+            {
+                Name = _name(infoBox)
+            };
+        }
+
+        // Find infobox that contains image and enemy stats
+        private static HtmlNode _infoBox(WebPage page)
+        {
+            var infoBox = page.Html.CssSelect(".portable-infobox");
+            return infoBox?.FirstOrDefault();
+        }
+
+        // Extract enemy name
+        private static string _name(HtmlNode infoBox)
+        {
+            var name = infoBox.CssSelect(".pi-title");
+            return name?.FirstOrDefault()?.InnerText;
+        }
+    }
 }
