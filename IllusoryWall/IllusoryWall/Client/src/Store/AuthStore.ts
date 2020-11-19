dfd73dbff4ff235a5080536d46b2f632ed63d286
link00000000 <crandall.logan@gmail.com>
2020-11-19 13:53:03 -0500

    Sync auth store with browsers localstorage

--- IllusoryWall/IllusoryWall/Client/src/Store/AuthStore.ts
+++ IllusoryWall/IllusoryWall/Client/src/Store/AuthStore.ts
@@ -1,4 +1,4 @@
-import ps, { Store } from 'pullstate'
+import { Store } from 'pullstate'
 
 export interface IAuthStore {
     authenticated: boolean
@@ -6,6 +6,61 @@ export interface IAuthStore {
     jwt?: string
 }
 
-export const AuthStore = new Store<IAuthStore>({
-    authenticated: false
-})
+const localStorageKey = 'IWAuth'
+
+export const AuthStore = (() => {
+    if (!window.localStorage) {
+        console.warn(
+            'Browser does not support localStorage, login will not persist between page refresh!'
+        )
+        return new Store<IAuthStore>({
+            authenticated: false
+        })
+    }
+
+    const serialAuthStore = window.localStorage.getItem(localStorageKey)
+    if (!serialAuthStore) {
+        console.log('No cached AuthStore found')
+        return new Store<IAuthStore>({
+            authenticated: false
+        })
+    }
+
+    let parsedAuthStore: { [key: string]: any | undefined }
+    try {
+        parsedAuthStore = JSON.parse(serialAuthStore)
+    } catch (error) {
+        console.error('Unable to parse stored AuthStore: ' + error.message)
+        return new Store<IAuthStore>({
+            authenticated: false
+        })
+    }
+
+    const { authenticated, username, jwt } = parsedAuthStore
+    if (!authenticated || !username || !jwt) {
+        console.error('Invalid AuthStore')
+        return new Store<IAuthStore>({
+            authenticated: false
+        })
+    }
+
+    return new Store<IAuthStore>({
+        authenticated,
+        username,
+        jwt
+    })
+})()
+
+AuthStore.createReaction(
+    (s) => s.authenticated,
+    (authenticated, _, original) => {
+        if (authenticated) {
+            window.localStorage.setItem(
+                localStorageKey,
+                JSON.stringify(original)
+            )
+        } else {
+            window.localStorage.removeItem(localStorageKey)
+        }
+    }
+)
