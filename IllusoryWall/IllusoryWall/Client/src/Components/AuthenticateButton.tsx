ef86203ad8e4f6ce5a21f57f0b6c256e2fe63fd7
link00000000 <crandall.logan@gmail.com>
2020-11-18 22:21:21 -0500

    Add authentication modals

--- IllusoryWall/IllusoryWall/Client/src/Components/AuthenticateButton.tsx
+++ IllusoryWall/IllusoryWall/Client/src/Components/AuthenticateButton.tsx
@@ -1,6 +1,11 @@
-import { Button } from 'antd'
+import { Button, Modal, Form, Input } from 'antd'
 import React, { FunctionComponent } from 'react'
 import styles from './AuthenticateButton.module.css'
+
+interface Credentials {
+    username: string
+    password: string
+}
 
 interface IProps {
     authenticated: boolean
@@ -13,14 +18,106 @@ interface IProps {
 export const AuthenticateButton: FunctionComponent<IProps> = (
     props: IProps
 ) => {
+    const [showLogin, setShowLogin] = React.useState<boolean>(false)
+    const [showRegistration, setShowRegistration] = React.useState<boolean>(
+        false
+    )
+
+    const [loginFormRef] = Form.useForm<Credentials>()
+    const [registerFromRef] = Form.useForm<Credentials>()
+
+    const handleLoginFormSubmit = async () => {
+        const credentials = (await loginFormRef.validateFields()) as Credentials
+        loginFormRef.resetFields()
+        setShowLogin(false)
+    }
+
+    const handleRegisterFormSubmit = async () => {
+        const credentials = (await registerFromRef.validateFields()) as Credentials
+        registerFromRef.resetFields()
+        setShowRegistration(false)
+    }
+
     return (
         <div className={styles['authenticate-button']}>
             {props.authenticated ? (
                 <Button>Logout</Button>
             ) : (
                 <>
-                    <Button type='text'>Login</Button>
-                    <Button type='primary'>Sign Up</Button>
+                    <Button
+                        type='text'
+                        onClick={() => {
+                            setShowLogin(true)
+                        }}
+                    >
+                        Login
+                    </Button>
+                    <Button
+                        type='primary'
+                        onClick={() => {
+                            setShowRegistration(true)
+                        }}
+                    >
+                        Sign Up
+                    </Button>
+
+                    <Modal
+                        visible={showLogin}
+                        title='Login'
+                        okText='Login'
+                        onCancel={() => {
+                            loginFormRef.resetFields()
+                            setShowLogin(false)
+                        }}
+                        onOk={handleLoginFormSubmit}
+                    >
+                        <Form form={loginFormRef} layout='vertical'>
+                            <Form.Item
+                                name='username'
+                                label='Username'
+                                rules={[{ required: true }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                name='password'
+                                label='Password'
+                                rules={[{ required: true }]}
+                            >
+                                <Input type='password' />
+                            </Form.Item>
+                        </Form>
+                    </Modal>
+
+                    <Modal
+                        visible={showRegistration}
+                        title='Sign Up'
+                        okText='Sign Up'
+                        onCancel={() => {
+                            registerFromRef.resetFields()
+                            setShowRegistration(false)
+                        }}
+                        onOk={handleRegisterFormSubmit}
+                    >
+                        <Form form={registerFromRef} layout='vertical'>
+                            <Form.Item
+                                name='username'
+                                label='Username'
+                                rules={[{ required: true }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item
+                                name='password'
+                                label='Password'
+                                rules={[{ required: true }]}
+                            >
+                                <Input type='password' />
+                            </Form.Item>
+                        </Form>
+                    </Modal>
                 </>
             )}
         </div>
